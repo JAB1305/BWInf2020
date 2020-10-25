7ec0342917352a4da55536230d253ff3ecacd140
Joshua Benning <jabenning13@gmail.com>
2020-10-25 11:01:05 +0100

    Edited README.md Changed variable handling and declarations

--- README.md
+++ README.md
@@ -4,20 +4,25 @@ Meine Lösungen und Dokumentation für den Bundeswettbewerb Informatik 2020
 ### Passwortgenerator für leicht zu merkende Passwörter. 
 > Schreibe einen eigenen Passwort-Generator. Dein Programm soll zufällige und sinnlose Zeichenfolgen ausgeben, die schwer zu erraten, aber einigermaßen gut auszusprechen und deshalb leichter zu merken sind.
 
-Quellcode: passwortgenerator.py
+Quellcode: passwortgenerator.py <br>
+Dokumentation: <a href="www.google.de">Dokumentation</a>
 ## Jugendaufgabe 2:
 ### Kartenauswertung um Bäue von Baulwürfen zu entdecken. 
 > Hilf dem Forschungsteam und schreibe ein Programm. Es soll die Hügelkarte einlesen und die Anzahl der Baulwurfsbaue ausgeben.
 
-Quellcode: baulwuerfe.py
+Quellcode: baulwuerfe.py<br>
+Dokumentation: <a href="www.google.de">Dokumentation</a>
+
 ## Aufgabe 1:
 ### Wörterrätsel lösen. 
 > Hilf den beiden und schreibe ein Programm, das einen Lückentext und eine Liste von Wörtern einliest und anschließend den vervollständigten Text ausgibt. Du kannst davon ausgehen, dass es nur eine richtige Lösung gibt.
 
-Quellcode: baulwuerfe.py
+Quellcode: woerter_sortieren.py<br>
+Dokumentation: <a href="www.google.de">Dokumentation</a>
 
 ## Aufgabe 5:
 ### Geschenke auf eine Klasse verteilen, so das möglichst viele Wünsche erfüllt werden. 
 > Schreibe ein Programm, das solche Wünsche von Schülerinnen und Schülern einliest und eine möglichst gute Verteilung gemäß der obigen Definition ausgibt.
 
-Quellcode: baulwuerfe.py
+Quellcode: wichteln.py<br>
+Dokumentation: <a href="www.google.de">Dokumentation</a>
--- wichteln.py
+++ wichteln.py
@@ -1,10 +1,6 @@
 from datetime import datetime
 
 
-def liste_rotieren(list):
-    return list[1:] + list[:1]
-
-
 def geschenke_verteilen(first_wishes, second_wishes, third_wishes, class_members):
     stats = {
         "first_wishes": 0,
@@ -12,11 +8,7 @@ def geschenke_verteilen(first_wishes, second_wishes, third_wishes, class_members
         "third_wishes": 0,
         "no_wish": 0
     }
-    old_stats = stats.copy()
-    pupils_without_gifts = []
-    pupils = []
-    for pupil_id in range(class_members):
-        pupils.append(pupil_id)
+    old_stats, pupils = stats.copy(), [pupil_id for pupil_id in range(class_members)]
     for pupil_id in range(class_members):
         stats = {
             "first_wishes": 0,
@@ -36,11 +28,10 @@ def geschenke_verteilen(first_wishes, second_wishes, third_wishes, class_members
                 relation[pupil] = third_wishes[pupil]
                 stats["third_wishes"] = stats["third_wishes"] + 1
             else:
-                pupils_without_gifts.append(pupil)
                 stats["no_wish"] = stats["no_wish"] + 1
         if is_better(old_stats, stats):
             old_stats = stats.copy()
-        pupils = liste_rotieren(pupils)
+        pupils = pupils[1:] + pupils[:1]
     return old_stats, relation
 
 
@@ -63,26 +54,21 @@ startTime = datetime.now()
 file = open(file_path, 'r', encoding='utf8')
 lines = file.readlines()
 class_members = int(lines[0])
-first_wishes = {}
-second_wishes = {}
-third_wishes = {}
+first_wishes, second_wishes, third_wishes = {}, {}, {}
 print("Geschenke werden zugeordnet ... \n\n")
 for pupil in range(class_members):  # Extract wishes from text file into lists
     wishes = lines[pupil + 1].split(" ")
     for i in range(len(wishes)):
         if i == 0 and wishes[i] == "":
             wishes.remove(wishes[i])
-    for i in range(len(wishes)):
         if i == 1 and wishes[i] == "":
             wishes.remove(wishes[i])
-    for i in range(len(wishes)):
         if i == 2 and wishes[i] == "":
             wishes.remove(wishes[i])
-    for i in range(len(wishes)):
+        if i == len(wishes):
+            break
         wishes[i] = wishes[i].replace("\n", "")
-    first_wishes[pupil] = wishes[0]
-    second_wishes[pupil] = wishes[1]
-    third_wishes[pupil] = wishes[2]
+    first_wishes[pupil], second_wishes[pupil], third_wishes[pupil] = wishes[0], wishes[1], wishes[2]
 solution, solution_dic = geschenke_verteilen(first_wishes, second_wishes, third_wishes, class_members)
 print("Erste Wünsche erfüllt: " + str(solution["first_wishes"]))
 print("Zweite Wünsche erfüllt: " + str(solution["second_wishes"]))
