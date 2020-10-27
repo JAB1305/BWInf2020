336062d8557e7a988dfb5e00c0d9bd222e7aa89d
JAB_1305 <jabenning13@gmail.com>
2020-10-27 15:48:57 +0100

    Started streichhölzer.py Added AppJar and implemented window for streichhölzer.py

---
+++ streichhölzer.py
@@ -0,0 +1,8 @@
+# Eigenes Format zur Darstellung dieser Streichholzrätsel
+
+from appJar import gui
+
+app = gui()
+app.addLabel("title", "Welcome to appJar")
+app.setLabelBg("title", "red")
+app.go()
