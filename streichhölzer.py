e8ad6460e6d7123ec629b8d8db2536192c268e84
Joshua Benning <jabenning13@gmail.com>
2020-10-27 20:21:05 +0100

    Added window handling to streichhölzer.py Added auto-generated gridsystems Worked on data handling for streichhölzer.py

--- streichhölzer.py
+++ streichhölzer.py
@@ -1,8 +1,40 @@
-# Eigenes Format zur Darstellung dieser Streichholzrätsel
+import pygame
+from datetime import datetime
 
-from appJar import gui
 
-app = gui()
-app.addLabel("title", "Welcome to appJar")
-app.setLabelBg("title", "red")
-app.go()
+file_path = "streichhölzer/streichhölzer" + input("Welches Beispiel soll getestet werden? 1, 2, 3, 4, 5, 6 oder 7?") + ".txt"
+startTime = datetime.now()
+file = open(file_path, 'r', encoding='utf8')
+lines = file.readlines()
+
+max_x = int(lines[0].split(",")[0]) + 1
+max_y = int(lines[0].split(",")[1]) + 1
+pygame.init()
+pause = False
+
+while not pause:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    DISPLAYSURF = pygame.display.set_mode((1600, 800))
+    WHITE = (255, 255, 255)
+    pygame.display.set_caption('Streichholzrätsel')
+    pygame.draw.rect(DISPLAYSURF, WHITE, (50, 50, 5, 700))
+    pygame.draw.rect(DISPLAYSURF, WHITE, (0, 700, 700, 5))
+    pygame.draw.rect(DISPLAYSURF, WHITE, (850, 50, 5, 700))
+    pygame.draw.rect(DISPLAYSURF, WHITE, (800, 700, 700, 5))
+    for x in range(max_x - 1):
+        x = x + 1
+        distance_to_left_border = x * (700 / max_x)
+        font = pygame.font.SysFont("Arial", 20)
+        text = font.render(str(x), True, WHITE)
+        DISPLAYSURF.blit(text, (distance_to_left_border - text.get_width() // 2, 720 - text.get_height() // 2))
+        DISPLAYSURF.blit(text, ((distance_to_left_border - text.get_width()) + 1600 // 2, 720 - text.get_height() // 2))
+    for y in range(max_y - 1):
+        y = y + 1
+        distance_to_top_border = y * (700 / max_y)
+        font = pygame.font.SysFont("Arial", 20)
+        text = font.render(str((y - max_y) * -1), True, WHITE)
+        DISPLAYSURF.blit(text, (20 - text.get_width() // 2, distance_to_top_border - text.get_height() // 2))
+        DISPLAYSURF.blit(text, (20 - text.get_width() + 1600 // 2, distance_to_top_border - text.get_height() // 2))
+    pygame.display.update()
